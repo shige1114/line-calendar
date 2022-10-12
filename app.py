8bbad30d1cf18e1a97332badff05137156113bef
shige1114 <g2054029@gmail.com>
2022-10-12 21:59:08 +0900

    tet

--- app.py
+++ app.py
@@ -50,8 +50,10 @@ help message
 """
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    bot_controller._bot_controller(event=event)
-
+    #bot_controller._bot_controller(event=event)
+    line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text))
     
 
     
