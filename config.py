675abde143c2e153eb7177a9222718ea9ec1b2d8
shige1114 <g2054029@gmail.com>
2022-10-16 21:20:54 +0900

    db

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 SECRET_KEY = '\xf7\xf4\x9bb\xd7\xa8\xdb\xee\x9f\xe3\x98SR\xda\xb0@\xb7\x12\xa4uB\xda\xa3\x1b'
 STRIPE_API_KEY = ''
 
-SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URI = "postgresql:"+":".join(os.environ["DATABASE_URL"].split(':')[1:])
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
