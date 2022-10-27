cb560e593a7a796d9ba161b939f323d2ee808445
shige1114 <g2054029@gmail.com>
2022-10-27 15:59:17 +0900

    test

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ import requests
 url = "https://line-chat-bot-1114.herokuapp.com/webview/event_view"
 url_local = "http://127.0.0.1:5000/webview/event_view"
 json_data = {
+
 	"room_id":"Cdf358fb1484640975bef1fee49ad3920"#jfladjfa
 }
-
-result = requests.post(url=url,json=json_data)
-print(result.text)
+headers = headers = {'content-type': 'application/json'}
+result = requests.post(url=url,json=json_data,headers=headers)
+print(result.json())
 
 "Cdf358fb1484640975bef1fee49ad3920"
