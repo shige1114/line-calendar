8ed774d6fc7b2ddae4fa1452975f9d3b1909a529
shige1114 <g2054029@gmail.com>
2022-10-30 00:34:50 +0900

    test

--- src/MVC/controller/BotController.py
+++ src/MVC/controller/BotController.py
@@ -131,6 +131,7 @@ class BotController:
             self.event,
             self.view._inform_vote_result(events)
         )
-        self.models._delete_calendar(room_id=self.room_id)
         self.models._delete_event(room_id=self.room_id)
+        self.models._delete_calendar(room_id=self.room_id)
+        
         
